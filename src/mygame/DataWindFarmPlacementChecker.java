f9f3d6b39ff0942a263a5c0bb0fd654205f10375
Ryan Moe <rmoe88@gmail.com>
2012-11-17 11:12:21 -0800

    Found a .gitignore solution that will work for our project

---
+++ src/mygame/DataWindFarmPlacementChecker.java
@@ -0,0 +1,19 @@
+package mygame;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.renderer.Camera;
+import com.jme3.scene.Node;
+
+/**
+ *
+ * @author Ryan
+ */
+public class DataWindFarmPlacementChecker {
+    
+    AssetManager assetMan;
+    Node rootNode;
+    DebugGlobals debugGlobals;
+    Camera gameCam;
+    ConfigReader configReader;
+    
+}
