2a6120648d942462c6895406c72d09bdf75a4309
Ryan Causey <rccausey@gmail.com>
2012-11-24 12:41:41 -0800

    Added comments to the beginning of DataSoundManager to more clearly define its purpose.

--- src/mygame/DataSoundManager.java
+++ src/mygame/DataSoundManager.java
@@ -7,6 +7,8 @@ import com.jme3.scene.Node;
 /**
  *
  * @author Ryan
+ * Data class for the SoundManager class
+ * Used to bring in the AssetManager, GUINode, and global debug information
  */
 public class DataSoundManager {
     
