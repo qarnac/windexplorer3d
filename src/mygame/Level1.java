615b76fdf820b02f6f7775f6c9e15eb74bdc29e7
Ryan Causey <nukesforbreakfast@Nukes-Laptop.(none)>
2013-03-13 12:07:26 -0700

    First commit of new class. Committing before attemping a refactor operation.

---
+++ src/mygame/Level1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.bullet.BulletAppState;
+
+/**
+ * Class to house all of the information necessary for level one and initialize
+ * the level
+ * @author nukesforbreakfast
+ */
+public class Level1 {
+    
+    private CameraController cameraControl;
+    private CameraPhysics cameraPhysics;
+    private Terrain terrainForLevel;
+    private SunController sunControl;
+    private BulletAppState physicsState;
+    
+    
+    public Level1()
+    
+}
